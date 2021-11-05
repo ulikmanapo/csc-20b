ac41f464401adcd53d954ff0c35bf4ce61c381dd
ulikmanapo <93367423+ulikmanapo@users.noreply.github.com>
2021-11-05 11:46:10 +0600

    Update index.php

--- index.php
+++ index.php
@@ -3,8 +3,7 @@
 <body>
 
 <h2>HTML Imagejhfjh</h2>
-<img src="<img src="" alt="Girl in a jacket" width="500" height="600">
-" alt="Girl in a jacket" width="500" height="600">
+<img src="https://www.elecspo.com/static/uploads/13/2018/10/playcsgoformoney-1440x810.jpg" width="500" height="600">
 
 </body>
 </html>
