073271b8c9675195cc7d1f15aa8c9a95ae61cae7
ulikmanapo <93367423+ulikmanapo@users.noreply.github.com>
2021-11-19 14:11:22 +0600

    Update index.php

--- index.php
+++ index.php
@@ -62,7 +62,7 @@
         <h1>Uluk Sakyev<br> CHEST AVTO </h1>
         <h3></h3>
         <p> .  .</p>
-        <img src='https://static.tildacdn.com/tild3465-6263-4338-a664-356664363933/113.png' alt='Uluk' style='width:500px;height:300px;'> 
+        <img src='https://static.tildacdn.com/tild3465-6263-4338-a664-356664363933/113.png' alt='Uluk' style='width:500px;height:300px; border-radius:50%;'> 
 	<form action='https://github.com/ulik' target='_blank'>
         <button class="button button1">CHEST AVTO</button>
         </form>
