48357172ca68b03926ccd3b268fc01e74d26c692
ulikmanapo <93367423+ulikmanapo@users.noreply.github.com>
2021-11-19 14:37:49 +0600

    Update index.php

--- index.php
+++ index.php
@@ -62,15 +62,15 @@
         <h1>Uluk Sakyev<br> CHEST AVTO </h1>
         <h3></h3>
         <p> .  .</p>
-        <img src='https://static.tildacdn.com/tild3465-6263-4338-a664-356664363933/113.png' alt='Uluk' style='width:200px;height:150px;  margin-left: 800px;border-radius:50%;'> 
+        <img src='https://www.youtube.com/channel/UCn1YQ4g_kKwJbt0MUVZfY7A' alt='Uluk' style='width:200px;height:150px;  margin-left: 800px;border-radius:50%;'> 
 	<form action='https://github.com/ulik' target='_blank'>
-        <button class="button button1">CHEST AVTO</button>
+        <button class="button button1">CHEST AVTO YOUTUBE</button>
         </form>
         <form action='https://instagram.com/sakyevv_?utm_medium=copy_link' target='_blank'>
         <button class="button button2">subscribe</button>
         </form>
-	<form action='https://https://chest-avto.ru/' target='_blank'>
-        <button class="button button2">SITE</button>
+	<form action='https://www.youtube.com/watch?v=ooxFCgGwMkc' target='_blank'>
+        <button class="button button2">PLAN B</button>
 	    <h4>Our adress:Bishkek. Ankara 103 <br>Phone number:+996501202404</h4>
     </body>
 </center>
