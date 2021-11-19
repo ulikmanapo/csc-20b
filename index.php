1307a85b552a0ae68e5b411e0bbdc391f2d64d31
ulikmanapo <93367423+ulikmanapo@users.noreply.github.com>
2021-11-19 13:07:18 +0600

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,74 @@
 <!DOCTYPE html>
 <html>
-<body>
 
-<h2>HTML Imagejhfjh</h2>
-<img src="https://www.elecspo.com/static/uploads/13/2018/10/playcsgoformoney-1440x810.jpg" width="500" height="600">
+<head>
+    <title>Uluk portfolio</title>
+</head>
 
-</body>
+<style>
+    h1 {
+        font-family:sans-serif ;
+    }
+    
+    h3 {
+        color: blue;
+        font-family: serif;
+    }
+    
+    p {
+        font-family:sans-serif ;
+    }
+    
+    .button {
+        border: none;
+        color: black;
+        padding: 16px 32px;
+        text-align: center;
+        text-decoration: none;
+        display: inline-block;
+        font-size: 16px;
+        margin: 4px 2px;
+        transition-duration: 0.4s;
+        cursor: pointer;
+    }
+    
+    .button1 {
+        background-color: black;
+        color: white;
+        border: 2px solid white;
+    }
+    /* Github */
+    
+    .button1:hover {
+        background-color: black;
+        color: white;
+    }
+    
+    .button2 {
+        background-color: white;
+        color: black;
+        border: 2px solid deeppink;
+    }
+    /* Instagram */
+    
+    .button2:hover {
+        background-color: deeppink;
+        color: white;
+    }
+</style>
+<center>
+
+    <body style="background-color:beige;">
+        <h1>Uluk Sakyev<br>Web junior Developer and designer</h1>
+        <h3>I am ULUK I like you)!</h3>
+        <p> Since beginning my journey nearly 6 years ago. I have developed amazing web applications.</p>
+        <img src='https://photos-kl.kcdn.kz/kolesa-read/736716e7fb56c116739002f0bc7c3687b03b1409-1193x671.jpg' alt='Uluk' style='width:200px;height:200px;  margin-left: 350px;'> 
+	<form action='https://github.com/zamlabek' target='_blank'>
+        <button class="button button1">Github</button>
+        </form>
+        <form action='https://instagram.com/abdr.gg?utm_medium=copy_link' target='_blank'>
+        <button class="button button2">log in</button>
+        </form>
+    </body>
+</center>
 </html>
