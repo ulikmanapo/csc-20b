24b559d8f9b34c972c9b73f741e2be90633c5ba1
ulikmanapo <93367423+ulikmanapo@users.noreply.github.com>
2021-11-19 13:42:56 +0600

    Update index.php

--- index.php
+++ index.php
@@ -60,7 +60,7 @@
 
     <body style="background-color:beige;">
         <h1>Uluk Sakyev<br>Web CHEST AVTO </h1>
-        <h3>ULIKMANAPO CAR DETEILING!</h3>
+        <h3>ULIKMANAPOo CAR DETEILING!</h3>
         <p> I sell Lexus lx600. I am perekupshi! .</p>
         <img src='https://photos-kl.kcdn.kz/kolesa-read/736716e7fb56c116739002f0bc7c3687b03b1409-1193x671.jpg' alt='Uluk' style='width:300px;height:300px;'> 
 	<form action='https://github.com/ulik' target='_blank'>
